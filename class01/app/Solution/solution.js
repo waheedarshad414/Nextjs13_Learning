2c4d52d73b9a8916913767cd2f93ed5223ff7077
waheedarshad414 <waheedarshad414@gmail.com>
2023-10-07 22:23:24 +0500

    update

---
+++ class01/app/Solution/solution.js
@@ -0,0 +1,10 @@
+function solution() {
+    return (
+        <div>
+            <h1>
+                Solution
+            </h1>
+        </div>
+    )
+}
+export default solution;
---
+++ class01/app/contact/page.js
@@ -0,0 +1,11 @@
+function contact() {
+    return (
+        <div>
+            <h1>
+                contact
+            </h1>
+        </div>
+    )
+}
+
+export default contact;
